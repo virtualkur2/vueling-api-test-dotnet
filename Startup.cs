ad4b3ea5a4dd722a4bd74cc39603e6d46360befe
Mauricio Contreras Canepa <virtual.kur2@gmail.com>
2019-02-23 23:43:20 +0100

    fixed error on service injection

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace vueling_api_test_netcore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<ITransactions, Transactions>();
-            services.AddSingleton<ITRates, Rates>();
+            services.AddSingleton<IRates, Rates>();
             services.AddMvc();
         }
 
