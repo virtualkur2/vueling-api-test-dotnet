682c2c0df147ab24048068cfe0422bc9bfb2625f
Mauricio Contreras Canepa <virtual.kur2@gmail.com>
2019-02-23 23:13:28 +0100

    added Transactions Model

---
+++ Models/Transactions.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class Transactions : ITransactions
+{
+  public string Get
+  {
+    get
+    {
+      Task<string> result = GetTransactions();
+      return result.Result;
+    }
+  }
+
+  public async Task<string> GetTransactions()
+  {
+    using (var client = new HttpClient())
+    {
+      try
+      {
+        client.BaseAddress = new Uri("http://quiet-stone-2094.herokuapp.com/");
+        var response = await client.GetAsync("transactions.json");
+        response.EnsureSuccessStatusCode();
+        string result = await response.Content.ReadAsStringAsync();
+        return result;
+      }
+      catch (HttpRequestException httpRequestException)
+      {
+        string message = httpRequestException.Message;
+        return "Error getting transactions: {message}";
+      }
+    }
+  }
+
+}
