5d752b14b564c51a694e4ceb952fb62d4dd5b11e
Mauricio Contreras Canepa <virtual.kur2@gmail.com>
2019-02-25 12:27:57 +0100

    testing with Data

--- Controllers/TransactionsController.cs
+++ Controllers/TransactionsController.cs
@@ -6,11 +6,12 @@ namespace vueling_api_test_netcore.Controllers
     [Route("api/[controller]")]
     public class TransactionsController : Controller
     {
+
       [HttpGet]
-        public string getTransactions()
-        {
-          Transactions _transactions = new Transactions();
-          return _transactions.Get;
-        }
+      public string getTransactions()
+      {
+        Transactions _transactions = new Transactions();
+        return _transactions.Get;
+      }
     }
 }
